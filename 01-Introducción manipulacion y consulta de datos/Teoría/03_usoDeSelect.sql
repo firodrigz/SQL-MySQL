585fee64684c7758fd27883dc4012098df37d6f9
firodrigz <firodrigz@gmail.com>
2023-10-12 10:04:35 -0300

    Consulta de datos

---
+++ 01-Introducción manipulacion y consulta de datos/Teoría/03_usoDeSelect.sql
@@ -0,0 +1,27 @@
+/*Consulta todos los elementos*/
+SELECT * FROM tbcliente;
+
+/*Consulta renombrando columnas*/
+SELECT NOMBRE AS NOMBRE_COMPLETO, DNI FROM tbcliente;
+
+/*Establece un límite de consulta*/
+SELECT NOMBRE, DNI FROM tbcliente LIMIT 6;
+
+/*Filtros*/
+/*Filtro específico*/
+SELECT * FROM tbproducto WHERE SABOR = 'Maracuyá';
+UPDATE tbproducto SET SABOR = 'Cítrico' WHERE SABOR = 'Limón';
+
+/*Filtro por condición*/
+SELECT * FROM tbcliente WHERE EDAD > 27;
+SELECT * FROM tbcliente WHERE NOMBRE > 'Erica Carvajo';
+
+/*Filtro por fecha*/
+SELECT * FROM tbcliente WHERE FECHA_NACIMIENTO < '1995-01-13';
+SELECT * FROM tbcliente WHERE DAY(FECHA_NACIMIENTO) = 20;
+
+/*Filtro de selección compuesta*/
+SELECT * FROM tbproducto WHERE PRECIO_LISTA BETWEEN 28.49 AND 28.52;
+SELECT * FROM tbproducto WHERE PRECIO_LISTA >= 28.49 AND PRECIO_LISTA <= 28.52;
+SELECT * FROM tbproducto WHERE ENVASE = 'Lata' OR ENVASE = 'Botella PET';
+SELECT * FROM tbproducto WHERE (PRECIO_LISTA >= 15 AND TAMANO = '1 Litro') OR (ENVASE = 'Lata' OR ENVASE = 'Botella PET');
